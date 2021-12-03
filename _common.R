f4fa69d7c9571637c063d7d6aad3897ad8f436f2
Youyi Fong <youyifong@gmail.com>
2021-12-02 17:51:42 -0800

    Update _common.R

--- _common.R
+++ _common.R
@@ -74,7 +74,7 @@ tmp=list(
         LLOQ = 117.35,
         ULOQ = 18976.19)
         ,
-    adcp=c( 
+    ADCP=c( 
         pos.cutoff=11.57,# as same lod
         LLOD = 11.57,
         ULOD = NA,
