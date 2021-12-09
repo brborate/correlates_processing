222d7575ad6def80f5937142c5de1ca311fd05f1
Bhavesh Borate <bborate@cosrv.pc.scharp.org>
2021-12-09 11:53:12 -0800

    workflow updates

--- riskscore_baseline/code/check_if_SL_needs_be_run.R
+++ riskscore_baseline/code/check_if_SL_needs_be_run.R
@@ -1,6 +1,6 @@
 # Check if only change in input dataset is marker data. 
 # If so, simply pull the earlier risk scores and add to the new processed dataset!
-if(study_name == "ENSEMBLE"){
+if(study_name == "ENSEMBLE" & attr(config, "config") == "janssen_pooled_real"){
     old_processed <- read.csv(here::here("..", "..", "previous_data_clean", paste0(attr(config, "config"), "_data_processed.csv"))) %>%
       select(Ptid, Riskscorecohortflag, Trt, all_of(endpoint), EthnicityHispanic,EthnicityNotreported, EthnicityUnknown,
              Black, Asian, NatAmer, PacIsl, Multiracial, Notreported, Unknown,
@@ -33,6 +33,44 @@ if(study_name == "ENSEMBLE"){
     }else{
       message("There is change in input data. Superlearner needs to be run and new risk scores generated!")
     }
+}
+
+
+# Append risk scores from janssen_pooled_real to janssen_pooled_realADCP
+if(study_name == "ENSEMBLE" & attr(config, "config") == "janssen_pooled_realADCP"){
+  real_processed <- read.csv(here::here("..", "data_clean", "janssen_pooled_real_data_processed_with_riskscore.csv")) %>%
+    select(Ptid, Riskscorecohortflag, Trt, all_of(endpoint), EthnicityHispanic,EthnicityNotreported, EthnicityUnknown,
+           Black, Asian, NatAmer, PacIsl, Multiracial, Notreported, Unknown,
+           URMforsubcohortsampling, HighRiskInd, HIVinfection, 
+           Sex, Age, BMI, Country, Region, CalendarDateEnrollment) 
+  
+  realADCP_processed <- inputFile %>% 
+    filter(Riskscorecohortflag == 1) %>%
+    select(Ptid, Riskscorecohortflag, Trt, all_of(endpoint), EthnicityHispanic,EthnicityNotreported, EthnicityUnknown,
+           Black, Asian, NatAmer, PacIsl, Multiracial, Notreported, Unknown,
+           URMforsubcohortsampling, HighRiskInd, HIVinfection, 
+           Sex, Age, BMI, Country, Region, CalendarDateEnrollment) 
+  
+  if(all.equal(real_processed, realADCP_processed)){
+    dat_with_riskscore <- inputFile %>% left_join(read.csv(here::here("..", "data_clean", "janssen_pooled_real_data_processed_with_riskscore.csv")) %>%
+                                                    select(Ptid, risk_score, standardized_risk_score), 
+                                                  by = "Ptid") %>%
+      filter(Riskscorecohortflag == 1) %>% 
+      drop_na(all_of(endpoint))
+    data_name_amended <- paste0(str_remove(paste0(attr(config, "config"), "_data_processed.csv"), ".csv"), "_with_riskscore")
+    
+    # Ensure all baseline negative and PP subjects have a risk score!
+    if(assertthat::assert_that(
+      all(!is.na(dat_with_riskscore %>% .$risk_score)), 
+      msg = "Some baseline negative and PP subjects have NA values in risk score!"
+    )){
+      write_csv(dat_with_riskscore,
+                here("..", "data_clean", paste0(data_name_amended, ".csv")))
+      message("No change in input data. Superlearner will not be run. Risk scores from earlier run appended to data_processed.csv!")
+    }
+  }else{
+    message("There is change in input data. Superlearner needs to be run and new risk scores generated!")
+  }
 }
 
 
