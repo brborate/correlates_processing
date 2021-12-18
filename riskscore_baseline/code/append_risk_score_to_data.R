46785d60d07667076823ef4b9bf1dd6dd4d95d56
Bhavesh Borate <bborate@cosrv.pc.scharp.org>
2021-12-17 18:10:17 -0800

    workflow updates related to multiple trials within study

--- riskscore_baseline/code/append_risk_score_to_data.R
+++ riskscore_baseline/code/append_risk_score_to_data.R
@@ -20,25 +20,9 @@ vaccinees_risk <- read.csv(here("output", "vaccine_ptids_with_riskscores.csv"))
 risk_scores <- rbind(placebos_risk, vaccinees_risk) %>%
   select(Ptid, risk_score, standardized_risk_score)
 inputFile_with_riskscore <- left_join(inputFile, risk_scores, by = "Ptid") 
-  #%>%
-  #filter(Riskscorecohortflag == 1) %>% 
-  #drop_na(all_of(endpoint))
-# data_name_amended <- paste0(str_remove(paste0(attr(config, "config"), "_data_processed.csv"), ".csv"), "_with_riskscore")
-
-# # Ensure all baseline negative and PP subjects have a risk score!
-# if(assertthat::assert_that(
-#   all(!is.na(inputFile_with_riskscore %>% .$risk_score)), 
-#           msg = "Some baseline negative and PP subjects have NA values in risk score!"
-# )){
-#   write_csv(inputFile_with_riskscore,
-#             here("..", "data_clean", paste0(data_name_amended, ".csv")))
-# }
 
 # Save inputFile 
 save(inputFile_with_riskscore, file = paste0("output/", attr(config, "config"), "_inputFile_with_riskscore.RData"))
-# inputFile_with_riskscore %>% 
-#   write.csv(here::here("..", "..", "..", "..", "covpn", "p3003", 
-#                        "analysis", "correlates", "Part_A_Blinded_Phase_Data", "adata", paste0(attr(config, "config"), "_data_processed_with_riskscoreNEW.csv"))) 
 
 # Create table of cases in both arms (post Risk score analyses)
 tab <- inputFile_with_riskscore %>%
