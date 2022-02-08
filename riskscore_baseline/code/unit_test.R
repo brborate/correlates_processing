193a8a36e3aeaab46fb646aba4490467459976aa
Youyi Fong <youyifong@gmail.com>
2022-02-07 19:12:00 -0800

    rm mock processed data files

--- riskscore_baseline/code/unit_test.R
+++ riskscore_baseline/code/unit_test.R
@@ -1,7 +1,10 @@
 library(digest)
 if(attr(config, "config") %in% c("janssen_pooled_mock", "moderna_mock")) {
   assertthat::assert_that(
-    digest(inputFile_with_riskscore)==ifelse(attr(config, "config")=="janssen_pooled_mock", "5cb73bf9810a11ae0b3d6638753bb690", "76bae5ccbfce93842efcd8b93fa3f38f"),
+    digest(inputFile_with_riskscore)==
+    ifelse(attr(config, "config")=="janssen_pooled_mock", 
+        "5cb73bf9810a11ae0b3d6638753bb690", 
+        "76bae5ccbfce93842efcd8b93fa3f38f"),
     msg = "failed sanity check")
   print("Passed sanity check")
 }
