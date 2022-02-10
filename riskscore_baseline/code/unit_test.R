57bd65b352ffd85a0aff46153cc90c286978b726
Bhavesh Borate <bborate@cosrv.pc.scharp.org>
2022-02-10 10:54:22 -0800

    ordered riskscore dataset before digest; updated digest code for both moderna and janssen mocks

--- riskscore_baseline/code/unit_test.R
+++ riskscore_baseline/code/unit_test.R
@@ -1,7 +1,9 @@
 library(digest)
 if(attr(config, "config") %in% c("janssen_pooled_mock", "moderna_mock") & Sys.getenv ("NOCHECK")=="") {
   assertthat::assert_that(
-    digest(inputFile_with_riskscore)==ifelse(attr(config, "config")=="janssen_pooled_mock", "2e68d1710090ba47dbf22420afb4dac7", "6e61dc1bedfae9969f4a377b13f1109a"),
+    digest(inputFile_with_riskscore[order(names(inputFile_with_riskscore))])==ifelse(attr(config, "config")=="janssen_pooled_mock", 
+                                                                                     "f7a8225eb5fa8cc9a5426211988b9d95", 
+                                                                                     "95368009ca10fc4b2e075885442e6e31"),
     msg = "failed sanity check: inputFile_with_riskscore_"%.%digest(inputFile_with_riskscore))
   print("Passed sanity check: inputFile_with_riskscore_"%.%digest(inputFile_with_riskscore))
 }
