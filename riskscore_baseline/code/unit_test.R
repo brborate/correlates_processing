c8db774c35564ef841c7320556c16ffee2bf61e2
Bhavesh Borate <bborate@cosrv.pc.scharp.org>
2022-02-10 11:05:49 -0800

    ordered riskscore dataset before digest; updated digest codes

--- riskscore_baseline/code/unit_test.R
+++ riskscore_baseline/code/unit_test.R
@@ -5,6 +5,6 @@ if(attr(config, "config") %in% c("janssen_pooled_mock", "moderna_mock") & Sys.ge
                                                                                        "f7a8225eb5fa8cc9a5426211988b9d95", 
                                                                                        "95368009ca10fc4b2e075885442e6e31"),
     msg = "failed sanity check: inputFile_with_riskscore_"%.%digest(inputFile_with_riskscore[order(names(inputFile_with_riskscore))]))
-  print("Passed sanity check: inputFile_with_riskscore_"%.%digest(inputFile_with_riskscore[order(names(inputFile_with_riskscore))]))
+  print("Passed sanity check: inputFile_with_riskscore_"%.%digest(inputFile_with_riskscore[order(names(inputFile_with_riskscore))])) 
 }
 
