06f2d1ffc060b9fef651fd8ca947edd2c661209d
Bhavesh Borate <bborate@cosrv.pc.scharp.org>
2023-07-28 10:57:26 -0700

    updated code slightly for moderna_boost so it can be run via make data_processed

--- riskscore_baseline/code/get_riskscores.R
+++ riskscore_baseline/code/get_riskscores.R
@@ -7,6 +7,7 @@
 # Sys.setenv(TRIAL = "vat08b") # Sanofi
 # Sys.setenv(TRIAL = "janssen_pooled_partA") 
 # Sys.setenv(TRIAL = "butantan")
+# Sys.setenv(TRIAL = "moderna_boost")
 
 print("GET_RISKSCORES.R")
 
