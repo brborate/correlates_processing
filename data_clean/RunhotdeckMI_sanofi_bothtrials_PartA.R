a654f7008a0fd8b6c21a96cf86e52c9cee576483
Youyi Fong <youyifong@gmail.com>
2024-07-22 21:01:56 -0700

    updates

--- data_clean/RunhotdeckMI_sanofi_bothtrials_PartA.R
+++ data_clean/RunhotdeckMI_sanofi_bothtrials_PartA.R
@@ -123,7 +123,7 @@ courseregion[TrialStage==2 & (Country==1 | Country==9)] <- 5
 courseregion[TrialStage==2 & (Country==6 | Country==2 | Country==10)] <- 6
 courseregion[TrialStage==2 & (Country==4 | Country==7)] <- 7
 
-Z1discrete <- as.matrix(cbind(dat[,"Trt"],courseregion))
+Z1discrete <- as.matrix(courseregion)
 # Turn off the effect of Z1scalar on the nearest neighbors, as Avscalar is much more relevant and should carry the weight
 Z1scalar <- matrix(rep(1,nrow(dat)),ncol=1)
 #Z2 <- dat[,"Day29pseudoneutid50"]
@@ -139,7 +139,9 @@ Avdiscrete <- matrix(rep(NA,length(epsilonv)),ncol=1)
 # Note "EventTimeFirstInfectionDate" is the date of the COVID-19 primary endpoint ignoring lineage information;
 # the primary endpoint of the study
 Numberdaysfromfirstpersonenrolleduntilprimaryendpoint <- as.Date(dat[,"EventTimeFirstInfectionDate"]) - as.Date(dat[,"FirstEnrollmentDate"])
-Avscalar <- matrix(Numberdaysfromfirstpersonenrolleduntilprimaryendpoint,ncol=1)  
+Avscalar <- matrix(Numberdaysfromfirstpersonenrolleduntilprimaryendpoint,ncol=1)
+keep <- Avscalar[,1] <= 189
+Z1discrete[keep,1] <- "99"
 epsilona <- Delta
 epsilona[is.na(Numberdaysfromfirstpersonenrolleduntilprimaryendpoint)] <- 0
 M <- 10
